7530fc06a2ace9aa0c4ca7a706327ffb16d28b69
thelaw2k <thelaw2k@gmail.com>
2023-05-21 21:50:05 -0300

    6.0

---
+++ 6/ProductManager.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  getProducts() {
+    if (!fs.existsSync(this.path)) {
+      return [];
+    }
+    try {
+      const fileData = fs.readFileSync(this.path, 'utf-8');
+      return JSON.parse(fileData);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  getProductById(productId) {
+    const products = this.getProducts();
+    for (const product of products) {
+      if (product.id === productId) {
+        return product;
+      }
+    }
+    return null;
+  }
+
+  _getNextId(products) {
+    if (products.length === 0) {
+      return 1;
+    } else {
+      return products[products.length - 1].id + 1;
+    }
+  }
+
+  _saveProducts(products) {
+    const data = JSON.stringify(products, null, 2);
+    fs.writeFileSync(this.path, data);
+  }
+}
+
+// Crear una instancia de ProductManager con la ruta del archivo
+const productManager = new ProductManager('products.json');
+
+// Obtener todos los productos
+const products = productManager.getProducts();
+console.log(products);
+
+// Obtener un producto por su ID
+const productId = 1;
+const product = productManager.getProductById(productId);
+console.log(product);
